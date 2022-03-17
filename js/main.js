4ad047e292c123268b9ba8ba34a8c7e0024c8fd4
Marius <marius@example.com>
2022-03-17 09:00:59 +0200

    getitng form value, ready to class

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('main');
+
+const formEl = document.forms[0];
+
+formEl.addEventListener('submit', (event) => {
+  // stabdom perkrovima ir issiutima per html
+  event.preventDefault();
+  console.log('js in controll');
+  const inputVal = formEl.elements.fullName.value;
+  console.log('inputVal ===', inputVal);
+});
